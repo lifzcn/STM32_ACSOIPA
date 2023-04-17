a7d6594bc4c1f6aeb6b58d1ccbd33e3914d8c678
LiFZ <lifzcn@qq.com>
2023-04-17 19:47:06 +0800

    Update some files

--- Project/Core/Src/main.c
+++ Project/Core/Src/main.c
@@ -124,24 +124,14 @@ int main(void)
 	OLED_ShowChinese(x + 16 + 16 * 3, y + 2 * 0, 3);
 	OLED_ShowChinese(x + 16 + 16 * 4, y + 2 * 0, 4);
 	OLED_ShowChinese(x + 16 + 16 * 5, y + 2 * 0, 5);
-//	OLED_ShowChinese(x + 16 + 16 * 0, y + 2 * 1, 6);
-//	OLED_ShowChinese(x + 16 + 16 * 1, y + 2 * 1, 7);
-//	OLED_ShowChinese(x + 16 + 16 * 2, y + 2 * 1, 8);
-//	OLED_ShowChinese(x + 16 + 16 * 3, y + 2 * 1, 9);
-//	OLED_ShowChinese(x + 16 + 16 * 4, y + 2 * 1, 10);
-//	OLED_ShowChinese(x + 16 + 16 * 5, y + 2 * 1, 11);
-	OLED_ShowChinese(x + 16 * 0, y + 2 * 1, 12);
-	OLED_ShowChinese(x + 16 * 1, y + 2 * 1, 13);
-	OLED_ShowChinese(x + 16 * 2, y + 2 * 1, 14);
-	OLED_ShowChinese(x + 16 * 3, y + 2 * 1, 15);
-	OLED_ShowChar(x + 16 * 3 + 8 * 2, y + 2 * 1, ':', 16);
-	OLED_ShowChinese(x + 16 * 0, y + 2 * 2, 12);
-	OLED_ShowChinese(x + 16 * 1, y + 2 * 2, 13);
-	OLED_ShowChinese(x + 16 * 2, y + 2 * 2, 16);
-	OLED_ShowChinese(x + 16 * 3, y + 2 * 2, 17);
-	OLED_ShowChar(x + 16 * 3 + 8 * 2, y + 2 * 2, ':', 16);
-	OLED_ShowChinese(x + 16 * 0, y + 2 * 3, 16);
-	OLED_ShowChinese(x + 16 * 1, y + 2 * 3, 17);
+	OLED_ShowChinese(x + 16 * 0, y + 2 * 1, 6);
+	OLED_ShowChinese(x + 16 * 1, y + 2 * 1, 7);
+	OLED_ShowChar(x + 16 * 2 + 8 * 0, y + 2 * 1, ':', 16);
+	OLED_ShowChinese(x + 16 * 0, y + 2 * 2, 8);
+	OLED_ShowChinese(x + 16 * 1, y + 2 * 2, 9);
+	OLED_ShowChar(x + 16 * 2 + 8 * 0, y + 2 * 2, ':', 16);
+	OLED_ShowChinese(x + 16 * 0, y + 2 * 3, 10);
+	OLED_ShowChinese(x + 16 * 1, y + 2 * 3, 11);
 	OLED_ShowChar(x + 16 * 2 + 8 * 0, y + 2 * 3, 'C', 16);
 	OLED_ShowChar(x + 16 * 2 + 8 * 1, y + 2 * 3, 's', 16);
 	OLED_ShowChar(x + 16 * 2 + 8 * 2, y + 2 * 3, ':', 16);
@@ -166,10 +156,10 @@ int main(void)
 		temperatureValue = DS18B20_GetTemperture();
 		temperatureIntegerValue = (int)temperatureValue;
 		temperatureDecimalValue = 10 * (temperatureValue - (int)temperatureValue);
-		OLED_ShowNum(x + 16 * 3 + 8 * 3, y + 2 * 2, temperatureIntegerValue, 2, 16);
-		OLED_ShowChar(x + 16 * 3 + 8 * 5, y + 2 * 2, '.', 16);
-		OLED_ShowNum(x + 16 * 3 + 8 * 6, y + 2 * 2, temperatureDecimalValue, 1, 16);
-		OLED_ShowChar(x + 16 * 3 + 8 * 7, y + 2 * 2, 'C', 16);
+		OLED_ShowNum(x + 16 * 2 + 8 * 1, y + 2 * 1, temperatureIntegerValue, 2, 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 3, y + 2 * 1, '.', 16);
+		OLED_ShowNum(x + 16 * 2 + 8 * 4, y + 2 * 1, temperatureDecimalValue, 1, 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 5, y + 2 * 1, 'C', 16);
 		
 		if(temperatureValue<temperatureLowLimitValue)//温度低于20度，报警并加热
 		{
@@ -203,23 +193,23 @@ int main(void)
 		}
 		
 		pressureValue = pressure;
-		OLED_ShowNum(x + 16 * 3 + 8 * 3, y + 2 * 2, pressureValue/1000, 6, 16);
-		OLED_ShowChar(x + 16 * 3 + 8 * 9, y + 2 * 2, 'k', 16);
-		OLED_ShowChar(x + 16 * 3 + 8 * 10, y + 2 * 2, 'P', 16);
-		OLED_ShowChar(x + 16 * 3 + 8 * 11, y + 2 * 2, 'a', 16);
+		OLED_ShowNum(x + 16 * 2 + 8 * 1, y + 2 * 2, pressureValue/1000, 6, 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 7, y + 2 * 2, 'k', 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 8, y + 2 * 2, 'P', 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 9, y + 2 * 2, 'a', 16);
 		
 		oxygenSolubilityValue = (pressure / standardAtmosphericPressureValue) * (477.8 / (temperatureValue + 32.26));
 		oxygenSolubilityIntegerValue = (int)oxygenSolubilityValue;
 		oxygenSolubilityDecimalValue_1 = 10 * (oxygenSolubilityValue - (int)oxygenSolubilityValue);
 		oxygenSolubilityDecimalValue_2 = 10 * (10 * (oxygenSolubilityValue - (int)oxygenSolubilityValue) - (int)(10 * (oxygenSolubilityValue - (int)oxygenSolubilityValue)));
-		OLED_ShowNum(x + 16 * 2 + 8 * 3, y + 2 * 3, oxygenSolubilityIntegerValue, 2, 16);
-		OLED_ShowChar(x + 16 * 2 + 8 * 5, y + 2 * 3, '.', 16);
-		OLED_ShowNum(x + 16 * 2 + 8 * 6, y + 2 * 3, oxygenSolubilityDecimalValue_1, 1, 16);
-		OLED_ShowNum(x + 16 * 2 + 8 * 7, y + 2 * 3, oxygenSolubilityDecimalValue_2, 1, 16);
-		OLED_ShowChar(x + 16 * 2 + 8 * 8, y + 2 * 3, 'm', 16);
-		OLED_ShowChar(x + 16 * 2 + 8 * 9, y + 2 * 3, 'g', 16);
-		OLED_ShowChar(x + 16 * 2 + 8 * 10, y + 2 * 3, '/', 16);
-		OLED_ShowChar(x + 16 * 2 + 8 * 11, y + 2 * 3, 'L', 16);
+		OLED_ShowNum(x + 16 * 2 + 8 * 1, y + 2 * 3, oxygenSolubilityIntegerValue, 2, 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 3, y + 2 * 3, '.', 16);
+		OLED_ShowNum(x + 16 * 2 + 8 * 4, y + 2 * 3, oxygenSolubilityDecimalValue_1, 1, 16);
+		OLED_ShowNum(x + 16 * 2 + 8 * 5, y + 2 * 3, oxygenSolubilityDecimalValue_2, 1, 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 6, y + 2 * 3, 'm', 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 7, y + 2 * 3, 'g', 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 8, y + 2 * 3, '/', 16);
+		OLED_ShowChar(x + 16 * 2 + 8 * 9, y + 2 * 3, 'L', 16);
 		
 		if(oxygenSolubilityValue<oxygenSolubilityLimitValue)//溶解氧低于5mg/L，步进电机转动
 		{
